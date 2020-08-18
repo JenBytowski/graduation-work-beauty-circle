c515a2d9142af57027b7040fb0643c015dacf92a
Yuheni Tabakerov <yuheni_tabakerov@strong-code.pro>
2020-08-18 12:12:17 +0300

    refactor swagger scheme

--- BC.API/Controllers/AuthenticationController.cs
+++ BC.API/Controllers/AuthenticationController.cs
@@ -1,59 +1,64 @@
-﻿using System.Threading.Tasks;
-using BC.API.Services.AuthenticationService;
+﻿using BC.API.Services.AuthenticationService;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace BC.API.Controllers
 {
-  [Route("authentication")]
-  [ApiController]
-  public class AuthenticationController : ControllerBase
-  {
-    readonly AuthenticationService _authenticationService;
-
-    public AuthenticationController(AuthenticationService authenticationService)
+    [Route("authentication")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
     {
-      _authenticationService = authenticationService;
-    }
+        readonly AuthenticationService _authenticationService;
+
+        public AuthenticationController(AuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
 
     [HttpPost]
     [AllowAnonymous]
+    [ApiExplorerSettings(GroupName = "authentication")]
     [Route("authenticate-by-vk")]
     public async Task<AuthenticationResponse> AuthentificatebyVK(string code)
     {
       return await _authenticationService.AuthenticatebyVK(code);
     }
 
-    [HttpPost]
-    [AllowAnonymous]
-    [Route("authenticate-by-instagram")]
-    public async Task<AuthenticationResponse> AuthentificatebyInstagram(string code)
-    {
-      return await _authenticationService.AuthenticatebyInstagram(code);
-    }
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("authenticate-by-instagram")]
+        [ApiExplorerSettings(GroupName = "authentication")]
+        public async Task<AuthenticationResponse> AuthentificatebyInstagram(string code)
+        {
+            return await _authenticationService.AuthenticatebyInstagram(code);
+        }
 
-    [HttpPost]
-    [AllowAnonymous]
-    [Route("authenticate-by-google")]
-    public async Task<AuthenticationResponse> AuthentificatebyGoogle(string code)
-    {
-      return await _authenticationService.AuthenticatebyGoogle(code);
-    }
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("authenticate-by-google")]
+        [ApiExplorerSettings(GroupName = "authentication")]
+        public async Task<AuthenticationResponse> AuthentificatebyGoogle(string code)
+        {
+            return await _authenticationService.AuthenticatebyGoogle(code);
+        }
 
-    [HttpPost]
-    [AllowAnonymous]
-    [Route("get-sms-authentication-code")]
-    public async Task GetSMSAuthentificationCode(string phone)
-    {
-      await _authenticationService.SendSMSAuthenticationCode(phone);
-    }
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("get-sms-authentication-code")]
+        [ApiExplorerSettings(GroupName = "authentication")]
+        public async Task GetSMSAuthentificationCode(string phone)
+        {
+            await _authenticationService.SendSMSAuthenticationCode(phone);
+        }
 
-    [HttpPost]
-    [AllowAnonymous]
-    [Route("authenticate-by-phone")]
-    public async Task<AuthenticationResponse> AuthentificatebyPhone(SMSCodeAuthenticationResponse model)
-    {
-      return await _authenticationService.AuthenticatebyPhone(model);
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("authenticate-by-phone")]
+        [ApiExplorerSettings(GroupName = "authentication")]
+        public async Task<AuthenticationResponse> AuthentificatebyPhone(SMSCodeAuthenticationResponse model)
+        {
+            return await _authenticationService.AuthenticatebyPhone(model);
+        }
     }
-  }
 }
--- BC.API/Controllers/WeatherForecastController.cs
+++ BC.API/Controllers/WeatherForecastController.cs
@@ -1,9 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace BC.API.Controllers
 {
@@ -14,7 +13,7 @@ namespace BC.API.Controllers
     private static readonly string[] Summaries = new[]
     {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+    };
 
     private readonly ILogger<WeatherForecastController> _logger;
 
@@ -23,17 +22,18 @@ namespace BC.API.Controllers
       _logger = logger;
     }
 
-    [HttpGet]
-    public IEnumerable<WeatherForecast> Get()
-    {
-      var rng = new Random();
-      return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-      {
-        Date = DateTime.Now.AddDays(index),
-        TemperatureC = rng.Next(-20, 55),
-        Summary = Summaries[rng.Next(Summaries.Length)]
-      })
-          .ToArray();
+        [HttpGet]
+        [ApiExplorerSettings(GroupName = "weather-forecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+                .ToArray();
+        }
     }
-  }
 }
