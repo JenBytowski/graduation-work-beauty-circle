8dede98313a7b8116b6f7e62e3482e7c935ec560
Timur Kuzmich <timur_kuzmich@strong-code.pro>
2020-09-21 16:14:21 +0300

    add routing animations

--- BC.Client/Client/src/app/app-routing.module.ts
+++ BC.Client/Client/src/app/app-routing.module.ts
@@ -1,41 +1,43 @@
 import {NgModule} from '@angular/core';
 import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
-import { MastersListComponent } from './masters/masters-list/masters-list.component';
+import {MastersListComponent} from './masters/masters-list/masters-list.component';
 import {MasterProfileComponent} from "./masters/master-profile/master-profile.component";
 import {AuthenticationComponent} from "./authentication/authentication/authentication.component";
 
 const routes: Routes = [
-    {
-        path: 'home',
-        loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
-    },
-    {
-      path: '',
-      redirectTo: 'home',
-      pathMatch: 'full'
-    },
-    {
-        path: 'authentication',
-        component: AuthenticationComponent,
-        pathMatch: 'full'
-    },
-    {
-        path: 'masters',
-        component: MastersListComponent,
-        pathMatch: 'full'
-    },
-    {
-        path: 'master/:id',
-        component: MasterProfileComponent,
-        pathMatch: 'full'
-    }
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'authentication',
+    component: AuthenticationComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'masters',
+    component: MastersListComponent,
+    pathMatch: 'full',
+    data: {animation: 'isLeft'}
+  },
+  {
+    path: 'master/:id',
+    component: MasterProfileComponent,
+    pathMatch: 'full',
+    data: {animation: 'isRight'}
+  }
 ];
 
 @NgModule({
-    imports: [
-        RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})
-    ],
-    exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {
 }
--- BC.Client/Client/src/app/app.component.ts
+++ BC.Client/Client/src/app/app.component.ts
@@ -1,21 +1,31 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {IonRouterOutlet, Platform} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
+import {RouterOutlet} from "@angular/router";
+import {slideInAnimation} from "./common/animations";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  styleUrls: ['app.component.scss'],
+  animations: [
+    slideInAnimation
+  ]
 })
 export class AppComponent {
+
   constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+      private platform: Platform,
+      private splashScreen: SplashScreen,
+      private statusBar: StatusBar
   ) {
     this.initializeApp();
+  }
+
+  prepareRoute(outlet: IonRouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
   }
 
   initializeApp() {
