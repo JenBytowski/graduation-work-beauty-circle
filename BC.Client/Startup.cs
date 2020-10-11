5790358f15dc0d384ac2e78345abfd1d7b9db3df
Aliaksandr Filipenka <aliaksandr_filipenka@strong-code.pro>
2020-10-11 20:51:14 +0300

    Small progress

--- BC.Client/Startup.cs
+++ BC.Client/Startup.cs
@@ -9,79 +9,83 @@ using Microsoft.Extensions.Hosting;
 
 namespace BC.Client
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllersWithViews();
-            // In production, the Angular files will be served from this directory
-            services.AddSpaStaticFiles(configuration => { configuration.RootPath = "Client/www"; });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            if (!env.IsDevelopment())
-            {
-                app.UseSpaStaticFiles();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller}/{action=Index}/{id?}");
-            });
-
-            app.UseSpa(spa =>
-            {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
-
-                spa.Options.SourcePath = "Client";
-                
-                spa.Options.StartupTimeout = new TimeSpan(0, 5, 0);
-
-                if (env.IsDevelopment())
-                {
-                    spa.UseAngularCliServer(npmScript: "start");
-                } else if (env.IsEnvironment("Timur"))
-                {
-                  spa.UseAngularCliServer(npmScript: "start-timur");
-                } else if (env.IsEnvironment("Roman"))
-                {
-                  spa.UseAngularCliServer(npmScript: "start-roman");
-                } else if (env.IsEnvironment("Wolk"))
-                {
-                  spa.UseAngularCliServer(npmScript: "start-wolk");
-                } else if (env.IsEnvironment("Sanya"))
-                {
-                  spa.UseAngularCliServer(npmScript: "start-sanya");
-                }
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddControllersWithViews();
+      // In production, the Angular files will be served from this directory
+      services.AddSpaStaticFiles(configuration => { configuration.RootPath = "Client/www"; });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+      }
+
+      app.UseHttpsRedirection();
+      app.UseStaticFiles();
+      if (!env.IsDevelopment())
+      {
+        app.UseSpaStaticFiles();
+      }
+
+      app.UseRouting();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllerRoute(
+          name: "default",
+          pattern: "{controller}/{action=Index}/{id?}");
+      });
+
+      app.UseSpa(spa =>
+      {
+        // To learn more about options for serving an Angular SPA from ASP.NET Core,
+        // see https://go.microsoft.com/fwlink/?linkid=864501
+
+        spa.Options.SourcePath = "Client";
+
+        spa.Options.StartupTimeout = new TimeSpan(0, 5, 0);
+
+        if (env.IsDevelopment())
+        {
+          spa.UseAngularCliServer(npmScript: "start");
+        }
+        else if (env.IsEnvironment("Timur"))
+        {
+          spa.UseAngularCliServer(npmScript: "start-timur");
+        }
+        else if (env.IsEnvironment("Roman"))
+        {
+          spa.UseAngularCliServer(npmScript: "start-roman");
+        }
+        else if (env.IsEnvironment("Wolk"))
+        {
+          spa.UseAngularCliServer(npmScript: "start-wolk");
+        }
+        else if (env.IsEnvironment("Sanya"))
+        {
+          spa.UseAngularCliServer(npmScript: "start-sanya");
+        }
+      });
+    }
+  }
 }
