95c258c322d5865e6072eb1f9e01bea06218764d
Timur Kuzmich <timur_kuzmich@strong-code.pro>
2020-10-23 16:30:17 +0300

    some fix

--- BC.APIGateway/Startup.cs
+++ BC.APIGateway/Startup.cs
@@ -17,22 +17,23 @@ namespace BC.APIGateway
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+      public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+      public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+      // This method gets called by the runtime. Use this method to add services to the container.
+      public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddControllers();
             services.AddOcelot(Configuration);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+      // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+      public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
