05b3d10b47fa48465791e9f5e4aadb6ad6d11909
Timur Kuzmich <timur_kuzmich@strong-code.pro>
2020-10-24 14:16:22 +0300

    add diff auth

--- BC.Common/workspace/projects/bc-master/src/app/authentication/authentication.module.ts
+++ BC.Common/workspace/projects/bc-master/src/app/authentication/authentication.module.ts
@@ -2,9 +2,9 @@ import {NgModule} from '@angular/core';
 import {CommonModule, DOCUMENT} from '@angular/common';
 
 import {AuthenticationRoutingModule} from './authentication-routing.module';
-import {AuthenticationComponent} from "./authentication/authentication.component";
 import {IonicModule} from "@ionic/angular";
 import {CookieService} from "ngx-cookie-service";
+import {AuthenticationComponent} from './authentication/authentication.component';
 
 export function getBaseUrl(document) {
   return document.getElementsByTagName('base')[0].href;
