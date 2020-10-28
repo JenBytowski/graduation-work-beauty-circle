5b5ec6cd36f77f93a8b69f13393ad12e332e1e56
Yuheni Tabakerov <yuheni_tabakerov@strong-code.pro>
2020-10-28 14:07:09 +0300

    refactor GetScheduleRes

--- BC.API/Services/BookingService/GetScheduleRes.cs
+++ BC.API/Services/BookingService/GetScheduleRes.cs
@@ -42,85 +42,61 @@ namespace BC.API.Services.BookingService
     
     public string ItemType { get; set; }
 
+    public Guid? ClientId { get; set; }
+
+    public Guid? ServiceTypeId { get; set; }
+
+    public string Description { get; set; }
+
+    public int? PriceMin { get; set; }
+
+    public int? PriceMax { get; set; }
+
+    public int? DurationInMinutesMax { get; set; }
+
     public static ScheduleDayItemRes ParseFromScheduleDayItem(ScheduleDayItem scheduleDayItem)
     {
       if (scheduleDayItem is Booking)
       {
-        return BookingRes.ParseFromBooking(scheduleDayItem as Booking);
+        var booking = scheduleDayItem as Booking;
+        
+        return new ScheduleDayItemRes
+        {
+          Id = booking.Id,
+          StartTime = booking.StartTime,
+          EndTime = booking.StartTime,
+          ItemType = nameof(Booking),
+          ClientId = booking.ClientId,
+          Description = booking.Description,
+          ServiceTypeId = booking.ServiceTypeId,
+          PriceMax = booking.PriceMax,
+          PriceMin = booking.PriceMin,
+          DurationInMinutesMax = booking.DurationInMinutesMax
+        };
       }
 
       if (scheduleDayItem is Pause)
       {
-        return PauseRes.ParseFromPause(scheduleDayItem as Pause);
+        var pause = scheduleDayItem as Pause;
+
+        return new ScheduleDayItemRes
+        {
+          Id = pause.Id,
+          StartTime = pause.StartTime,
+          EndTime = pause.EndTime,
+          ItemType = nameof(Pause),
+          Description = pause.Description,
+        };
       }
 
-      return WindowRes.ParseFromWindow(scheduleDayItem as Window);
-    }
-  }
-
-  public class PauseRes : ScheduleDayItemRes
-  {
-    public string Description { get; set; }
-
-    public static PauseRes ParseFromPause(Pause pause)
-    {
-      return new PauseRes
+      return new ScheduleDayItemRes
       {
-        Id = pause.Id,
-        StartTime = pause.StartTime,
-        EndTime = pause.EndTime,
-        ItemType = nameof(Pause),
-        Description = pause.Description,
+        Id = scheduleDayItem.Id,
+        StartTime = scheduleDayItem.StartTime,
+        EndTime = scheduleDayItem.EndTime,
+        ItemType = nameof(Window),
       };
     }
   }
-
-  public class BookingRes : ScheduleDayItemRes
-  {
-    public Guid ClientId { get; set; }
-
-    public Guid ServiceTypeId { get; set; }
-
-    public string ServiceTypeName { get; set; }
-
-    public string Description { get; set; }
-
-    public int PriceMin { get; set; }
-
-    public int PriceMax { get; set; }
-
-    public int DurationInMinutesMax { get; set; }
-
-    public static BookingRes ParseFromBooking(Booking booking)
-    {
-      return new BookingRes
-      {
-        Id = booking.Id,
-        ClientId = booking.ClientId,
-        StartTime = booking.StartTime,
-        EndTime = booking.EndTime,
-        ItemType = nameof(Booking),
-        ServiceTypeId = booking.ServiceTypeId,
-        ServiceTypeName = booking.ServiceTypeName,
-        DurationInMinutesMax = booking.DurationInMinutesMax,
-        PriceMax = booking.PriceMax,
-        PriceMin = booking.PriceMin,
-        Description = booking.Description
-      };
-    }
-  }
-
-  public class WindowRes : ScheduleDayItemRes
-  {
-    public static WindowRes ParseFromWindow(Window window)
-    {
-      return new WindowRes
-      {
-        Id = window.Id, 
-        StartTime = window.StartTime,
-        EndTime = window.EndTime,
-        ItemType = nameof(Window)
-      };
-    }
-  }
+  
 }
