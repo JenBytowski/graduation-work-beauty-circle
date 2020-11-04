8385d3d62b8803123d82ed697c6d3d0a86ec997d
Yuheni Tabakerov <yuheni_tabakerov@strong-code.pro>
2020-11-04 21:34:07 +0300

    fix migrations in MasterList service

---
+++ BC.API/Services/MastersListService/Data/Migrations/20201104183205_Add_IsPublish_Field.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BC.API.Services.MastersListService.Data.Migrations
+{
+    public partial class Add_IsPublish_Field : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPublish",
+                schema: "masters",
+                table: "Masters",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPublish",
+                schema: "masters",
+                table: "Masters");
+        }
+    }
+}
