38e596eaf832ab25617fb8ead59eb35739ec4712
Yuheni Tabakerov <yuheni_tabakerov@strong-code.pro>
2020-11-12 00:19:25 +0300

    add methods to process images

--- BC.API/Services/MastersListService/AvatarImageProcessingSaga.cs
+++ BC.API/Services/MastersListService/AvatarImageProcessingSaga.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Mime;
 using System.Threading.Tasks;
 using BC.API.Services.MastersListService.Data;
 using Microsoft.EntityFrameworkCore;
@@ -16,11 +23,13 @@ namespace BC.API.Services.MastersListService
     
     private readonly IEventBus _eventBus;
     private readonly MastersContext _context;
+    private readonly MastersListServiceConfig _config;
 
-    public AvatarImageProcessingSaga(IEventBus eventBus, MastersContext context)
+    public AvatarImageProcessingSaga(IEventBus eventBus, MastersContext context, MastersListServiceConfig config)
     {
       _eventBus = eventBus;
       _context = context;
+      _config = config;
     }
 
     public void Start(Guid masterId)
@@ -60,6 +69,65 @@ namespace BC.API.Services.MastersListService
       master.ThumbnailFileName = master.AvatarSourceFileName;
       await this._context.SaveChangesAsync();
       Console.WriteLine("Create Thumbnail");
+    }
+
+    private Bitmap ProcessImage(Stream imageStream, int sideLength)
+    {
+      var image = (Bitmap)Image.FromStream(imageStream);
+      
+      if (image.Width / image.Height != 1)
+      {
+        image = CropImageToSquare(image);
+      }
+
+      return ResizeImage(image, new Size {Width = sideLength, Height = sideLength});
+    }
+
+    private Bitmap CropImageToSquare(Bitmap image)
+    {
+      var delta = Math.Abs(image.Width - image.Height);
+
+      if (image.Width > image.Height)
+      {
+        return image.Clone(new Rectangle(delta / 2, 0, image.Width - delta, image.Height),
+          image.PixelFormat);
+      }
+
+      return image.Clone(new Rectangle(0, delta / 2, image.Width, image.Height - delta), image.PixelFormat);
+    }
+    
+    private Bitmap ResizeImage(Image imgToResize, Size size)
+    {
+      var sourceWidth = imgToResize.Width;
+      var sourceHeight = imgToResize.Height;
+
+      var nPercent = 0f;
+      var nPercentW = 0f;
+      var nPercentH = 0f;
+
+      nPercentW = (float)size.Width / sourceWidth;
+      nPercentH = (float)size.Height / sourceHeight;
+
+      if (nPercentH < nPercentW)
+      {
+        nPercent = nPercentH;
+      }
+      else
+      {
+        nPercent = nPercentW;
+      }
+
+      var destWidth = (int)(sourceWidth * nPercent);
+      var destHeight = (int)(sourceHeight * nPercent);
+
+      var result = new Bitmap(destWidth, destHeight);
+      Graphics graphics = Graphics.FromImage(result);
+
+      graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+      graphics.DrawImage(imgToResize, 0, 0, destWidth, destHeight);
+      graphics.Dispose();
+
+      return result;
     }
 
     public void Dispose()
