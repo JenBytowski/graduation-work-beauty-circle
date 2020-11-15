9c8d530bdf6c49dc907912e14cc0b18f4e1db075
Aliaksandr Filipenka <aliaksandr_filipenka@strong-code.pro>
2020-11-15 19:40:16 +0300

    Init interservice communication question

---
+++ BC.API/Services/FeedbackService/FeedbackService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BC.API.Services.FeedbackService.Data;
+
+namespace BC.API.Services.FeedbackService
+{
+  public class FeedbackService
+  {
+    private readonly FeedbackContext _context;
+
+    public FeedbackService(FeedbackContext context)
+    {
+      _context = context;
+    }
+    
+    public async Task PostBookingFeedback(PostBookingFeedbackReq req)
+    {
+      
+    }
+
+    public async Task<IEnumerable<BookingFeedbackRes>> GetBookingFeedbacks(GetBookingFeedbacksReq req)
+    {
+      return new[] {new BookingFeedbackRes()};
+    }
+    
+  }
+
+  public class GetBookingFeedbacksReq
+  {
+    public Guid BookingId { get; set; }
+  }
+
+  public class BookingFeedbackRes
+  {
+  }
+
+  public class PostBookingFeedbackReq
+  {
+    public Guid BookingId { get; set; }
+    public int Stars { get; set; }
+    public string Text { get; set; }
+  }
+}
