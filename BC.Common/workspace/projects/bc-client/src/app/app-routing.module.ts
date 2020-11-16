e60a33ee582cbf1fa80800bbf9cbd33179c1d6e9
Timur Kuzmich <timur_kuzmich@strong-code.pro>
2020-11-16 12:52:02 +0300

    add guards

--- BC.Common/workspace/projects/bc-client/src/app/app-routing.module.ts
+++ BC.Common/workspace/projects/bc-client/src/app/app-routing.module.ts
@@ -4,34 +4,42 @@ import {AuthenticationComponent} from "./authentication/authentication/authentic
 import {MastersListComponent} from "./masters/masters-list/masters-list.component";
 import {MasterProfileComponent} from "./masters/master-profile/master-profile.component";
 import {HomePage} from "./home/home.page";
-import {TokenStoreService} from "bc-common";
+import {TokenGuard} from "./guards/masters.guard";
+import {NotFoundPageComponent} from "./exeption/not-found-page/not-found-page.component";
+import {AuthenticationGuard} from "./guards/authentication.guard";
+import {UnauthorizedPageComponent} from "./exeption/unauthorized-page/unauthorized-page.component";
 
-const tokenStoreService: TokenStoreService = new TokenStoreService();
 const routes: Routes = [
   {
-    path: 'home',
-    component: HomePage,
-    pathMatch: 'full'
+    path: '',
+    component: HomePage
   },
   {
     path: 'authentication',
     component: AuthenticationComponent,
-    pathMatch: 'full'
+    pathMatch: 'full',
+    canActivate: [AuthenticationGuard]
   },
   {
     path: 'masters',
     component: MastersListComponent,
-    pathMatch: 'full'
+    pathMatch: 'full',
+    canActivate: [TokenGuard]
   },
   {
     path: 'master/:id',
     component: MasterProfileComponent,
+    pathMatch: 'full',
+    canActivate: [TokenGuard]
+  },
+  {
+    path: 'unauthorized',
+    component: UnauthorizedPageComponent,
     pathMatch: 'full'
   },
   {
-    path: '',
-    redirectTo: tokenStoreService.get() ? 'masters' : 'authentication',
-    pathMatch: 'full'
+    path: '**',
+    component: NotFoundPageComponent
   }
 ];
 
