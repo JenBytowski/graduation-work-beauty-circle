e289798f5c41683bd9f10dd7c4a38e1ecbc186a5
Timur Kuzmich <timur_kuzmich@strong-code.pro>
2020-12-06 15:37:06 +0300

    add filtre

--- BC.Common/workspace/projects/bc-client/src/app/masters/masters-list/masters-list.component.ts
+++ BC.Common/workspace/projects/bc-client/src/app/masters/masters-list/masters-list.component.ts
@@ -12,9 +12,21 @@ export class MastersListComponent implements OnInit {
 
   @ViewChild("topMenu", {static: false})
   topMenu: ElementRef;
-
   @ViewChild("filter", {static: false})
   filter: ElementRef;
+
+  @ViewChild('cityId', {static: false})
+  cityId: ElementRef;
+  @ViewChild('serviceTypes', {static: false})
+  serviceTypes: ElementRef;
+  @ViewChild('startHour', {static: false})
+  startHour: ElementRef;
+  @ViewChild('endHour', {static: false})
+  endHour: ElementRef;
+  @ViewChild('skip', {static: false})
+  skip: ElementRef;
+  @ViewChild('take', {static: false})
+  take: ElementRef;
 
   public filterStatus: boolean = false;
   public vm: Vm = new Vm();
@@ -27,13 +39,38 @@ export class MastersListComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.vm.filtre = new Filtre();
     this.tokenStore.get() ? this.isAuthorized = true : this.isAuthorized = false;
     console.log(this.isAuthorized);//
-    this.masterList.getMasters(null, null, null, null, 0, 10).subscribe(data => {
+    this.improveGetMasters(this.vm.filtre);
+  }
+
+  public regetMasters(){
+    this.fillFiltre();
+    this.improveGetMasters(this.vm.filtre);
+  }
+
+  public improveGetMasters(filtre: Filtre){
+    this.masterList.getMasters(
+      filtre.cityId,
+      filtre.serviceTypeIds,
+      filtre.startHour,
+      filtre.endHour,
+      filtre.skip,
+      filtre.take).subscribe(data => {
       data.forEach(item => (item as any).starRating = this.countStarRating(item.averageRating));
       this.vm.Masters = data;
       console.log(data);//
     });
+  }
+
+  public fillFiltre(){
+    this.vm.filtre.cityId = (this.cityId as any).el.value ? (this.cityId as any).el.value.toString() : undefined;
+    this.vm.filtre.serviceTypeIds = (this.serviceTypes as any).el.value ? (this.serviceTypes as any).el.value.toString() : undefined;
+    this.vm.filtre.startHour = (this.startHour as any).el.value ? (this.startHour as any).el.value.toString() : undefined;
+    this.vm.filtre.endHour = (this.endHour as any).el.value ? (this.endHour as any).el.value.toString() : undefined;
+    this.vm.filtre.skip = (this.skip as any).el.value ? (this.skip as any).el.value.toString() : undefined;
+    this.vm.filtre.take = (this.take as any).el.value ? (this.take as any).el.value.toString() : undefined;
   }
 
   public logScrollStart() {
@@ -70,4 +107,14 @@ export class MastersListComponent implements OnInit {
 
 class Vm {
   public Masters: any;
+  public filtre: Filtre;
+}
+
+class Filtre {
+  public cityId: string | null | undefined;
+  public serviceTypeIds: string[] | null | undefined;
+  public startHour: number | null | undefined;
+  public endHour: number | null | undefined;
+  public skip: number | undefined;
+  public take: number | undefined;
 }
