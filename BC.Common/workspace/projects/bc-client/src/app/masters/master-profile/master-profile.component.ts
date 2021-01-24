87adf3d015c859a0fb65bb5d97ff89a7293942c5
Timur Kuzmich <oneghostgream@gmail.com>
2021-01-24 12:40:43 +0300

    Add some changes in client

--- BC.Common/workspace/projects/bc-client/src/app/masters/master-profile/master-profile.component.ts
+++ BC.Common/workspace/projects/bc-client/src/app/masters/master-profile/master-profile.component.ts
@@ -1,24 +1,31 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {MasterListClient} from "bc-common";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MasterListClient, BookingClient, JWTDecodeService, TokenStoreService} from 'bc-common';
 
 @Component({
   selector: 'app-master-profile',
   templateUrl: './master-profile.component.html',
-  styleUrls: ['./master-profile.component.css']
+  styleUrls: ['./master-profile.component.css'],
 })
 export class MasterProfileComponent implements OnInit {
-
   public vm: Vm = new Vm();
 
-  constructor(private route: ActivatedRoute, private masterList: MasterListClient.MasterListClient) {
-  }
+  constructor(
+    private route: ActivatedRoute,
+    private masterList: MasterListClient.MasterListClient,
+    private booking: BookingClient.BookingClient,
+    private tokenStore: TokenStoreService,
+    private jwtdecode: JWTDecodeService
+  ) {}
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
-    this.masterList.getMasterById(id).subscribe(data => {
+    this.masterList.getMasterById(id).subscribe((data) => {
       (data as any).starRating = this.countStarRating(data.averageRating);
       this.vm.Master = data;
+      this.vm.ClientId = this.jwtdecode.decode(this.tokenStore.get())[
+        'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier'
+      ];
       console.log(data);
     });
   }
@@ -26,15 +33,48 @@ export class MasterProfileComponent implements OnInit {
   public countStarRating(rating: number) {
     rating = Math.round(rating * 2) / 2;
     return [
-      (rating >= 1 ? 'star' : rating >= 0.5 ? 'star-half-outline' : 'star-outline'),
-      (rating >= 2 ? 'star' : rating >= 1.5 ? 'star-half-outline' : 'star-outline'),
-      (rating >= 3 ? 'star' : rating >= 2.5 ? 'star-half-outline' : 'star-outline'),
-      (rating >= 4 ? 'star' : rating >= 3.5 ? 'star-half-outline' : 'star-outline'),
-      (rating >= 5 ? 'star' : rating >= 4.5 ? 'star-half-outline' : 'star-outline'),
+      rating >= 1
+        ? 'star'
+        : rating >= 0.5
+        ? 'star-half-outline'
+        : 'star-outline',
+      rating >= 2
+        ? 'star'
+        : rating >= 1.5
+        ? 'star-half-outline'
+        : 'star-outline',
+      rating >= 3
+        ? 'star'
+        : rating >= 2.5
+        ? 'star-half-outline'
+        : 'star-outline',
+      rating >= 4
+        ? 'star'
+        : rating >= 3.5
+        ? 'star-half-outline'
+        : 'star-outline',
+      rating >= 5
+        ? 'star'
+        : rating >= 4.5
+        ? 'star-half-outline'
+        : 'star-outline',
     ];
+  }
+
+  public addBooking() {
+    let req: BookingClient.AddBookingReq = new BookingClient.AddBookingReq();
+    req.masterId = this.vm.Master.id;
+    req.clientId = this.vm.ClientId;
+    req.serviceType = null;
+    req.description = 'Description';
+    req.startTime = new Date(0);
+    req.endTime = new Date(10000);
+    console.log(req);//
+    this.booking.addBooking(req).subscribe((data) => console.log(data));
   }
 }
 
 class Vm {
   public Master: any;
+  public ClientId: string;
 }
--- BC.Common/workspace/projects/bc-client/src/app/masters/masters-list/masters-list.component.ts
+++ BC.Common/workspace/projects/bc-client/src/app/masters/masters-list/masters-list.component.ts
@@ -1,31 +1,28 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {MenuController} from '@ionic/angular';
-import {MasterListClient, TokenStoreService} from "bc-common";
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { MasterListClient, TokenStoreService } from 'bc-common';
 
 @Component({
   selector: 'app-masters-list',
   templateUrl: './masters-list.component.html',
-  styleUrls: ['./masters-list.component.css']
+  styleUrls: ['./masters-list.component.css'],
 })
-
 export class MastersListComponent implements OnInit {
-
-  @ViewChild("topMenu", {static: false})
+  @ViewChild('topMenu', { static: false })
   topMenu: ElementRef;
-  @ViewChild("filter", {static: false})
+  @ViewChild('filter', { static: false })
   filter: ElementRef;
 
-  @ViewChild('cityId', {static: false})
+  @ViewChild('cityId', { static: false })
   cityId: ElementRef;
-  @ViewChild('serviceTypes', {static: false})
+  @ViewChild('serviceTypes', { static: false })
   serviceTypes: ElementRef;
-  @ViewChild('startHour', {static: false})
+  @ViewChild('startHour', { static: false })
   startHour: ElementRef;
-  @ViewChild('endHour', {static: false})
+  @ViewChild('endHour', { static: false })
   endHour: ElementRef;
-  @ViewChild('skip', {static: false})
+  @ViewChild('skip', { static: false })
   skip: ElementRef;
-  @ViewChild('take', {static: false})
+  @ViewChild('take', { static: false })
   take: ElementRef;
 
   public filterStatus: boolean = false;
@@ -33,47 +30,68 @@ export class MastersListComponent implements OnInit {
   public isAuthorized: boolean;
 
   constructor(
-    private menu: MenuController,
     private masterList: MasterListClient.MasterListClient,
-    private tokenStore: TokenStoreService) {
-  }
+    private tokenStore: TokenStoreService
+  ) {}
 
   ngOnInit(): void {
     this.vm.filtre = new Filtre();
     this.fillFiltre();
     console.log(this.vm);
-    this.tokenStore.get() ? this.isAuthorized = true : this.isAuthorized = false;
-    console.log(this.isAuthorized);//
+    this.tokenStore.get()
+      ? (this.isAuthorized = true)
+      : (this.isAuthorized = false);
+    console.log(this.isAuthorized); //
     this.improveGetMasters(this.vm.filtre);
   }
 
-  public regetMasters(){
+  public regetMasters() {
     this.fillFiltre();
     console.log(this.vm);
     this.improveGetMasters(this.vm.filtre);
   }
 
-  public improveGetMasters(filtre: Filtre){
-    this.masterList.getMasters(
-      filtre.cityId,
-      filtre.serviceTypeIds,
-      filtre.startHour,
-      filtre.endHour,
-      filtre.skip,
-      filtre.take).subscribe(data => {
-      data.forEach(item => (item as any).starRating = this.countStarRating(item.averageRating));
-      this.vm.Masters = data;
-      console.log(data);//
-    });
+  public improveGetMasters(filtre: Filtre) {
+    this.masterList
+      .getMasters(
+        filtre.cityId,
+        filtre.serviceTypeIds,
+        filtre.startHour,
+        filtre.endHour,
+        filtre.skip,
+        filtre.take
+      )
+      .subscribe((data) => {
+        data.forEach(
+          (item) =>
+            ((item as any).starRating = this.countStarRating(
+              item.averageRating
+            ))
+        );
+        this.vm.Masters = data;
+        console.log(data); //
+      });
   }
 
-  public fillFiltre(){
-    this.vm.filtre.cityId = (this.cityId as any)?.el.value ? (this.cityId as any).el.value.toString() : null;
-    this.vm.filtre.serviceTypeIds = (this.serviceTypes as any)?.el.value ? (this.serviceTypes as any).el.value.toString().split(' ') : null;
-    this.vm.filtre.startHour = (this.startHour as any)?.el.value ? (this.startHour as any).el.value.toString() : null;
-    this.vm.filtre.endHour = (this.endHour as any)?.el.value ? (this.endHour as any).el.value.toString() : null;
-    this.vm.filtre.skip = (this.skip as any)?.el.value ? (this.skip as any).el.value.toString() : 0;
-    this.vm.filtre.take = (this.take as any)?.el.value ? (this.take as any).el.value.toString() : 10;
+  public fillFiltre() {
+    this.vm.filtre.cityId = (this.cityId as any)?.el.value
+      ? (this.cityId as any).el.value.toString()
+      : null;
+    this.vm.filtre.serviceTypeIds = (this.serviceTypes as any)?.el.value
+      ? (this.serviceTypes as any).el.value.toString().split(' ')
+      : null;
+    this.vm.filtre.startHour = (this.startHour as any)?.el.value
+      ? (this.startHour as any).el.value.toString()
+      : null;
+    this.vm.filtre.endHour = (this.endHour as any)?.el.value
+      ? (this.endHour as any).el.value.toString()
+      : null;
+    this.vm.filtre.skip = (this.skip as any)?.el.value
+      ? (this.skip as any).el.value.toString()
+      : 0;
+    this.vm.filtre.take = (this.take as any)?.el.value
+      ? (this.take as any).el.value.toString()
+      : 10;
   }
 
   public logScrollStart() {
@@ -99,15 +117,35 @@ export class MastersListComponent implements OnInit {
   public countStarRating(rating: number) {
     rating = Math.round(rating * 2) / 2;
     return [
-      (rating >= 1 ? 'star' : rating >= 0.5 ? 'star-half-outline' : 'star-outline'),
-      (rating >= 2 ? 'star' : rating >= 1.5 ? 'star-half-outline' : 'star-outline'),
-      (rating >= 3 ? 'star' : rating >= 2.5 ? 'star-half-outline' : 'star-outline'),
-      (rating >= 4 ? 'star' : rating >= 3.5 ? 'star-half-outline' : 'star-outline'),
-      (rating >= 5 ? 'star' : rating >= 4.5 ? 'star-half-outline' : 'star-outline'),
+      rating >= 1
+        ? 'star'
+        : rating >= 0.5
+        ? 'star-half-outline'
+        : 'star-outline',
+      rating >= 2
+        ? 'star'
+        : rating >= 1.5
+        ? 'star-half-outline'
+        : 'star-outline',
+      rating >= 3
+        ? 'star'
+        : rating >= 2.5
+        ? 'star-half-outline'
+        : 'star-outline',
+      rating >= 4
+        ? 'star'
+        : rating >= 3.5
+        ? 'star-half-outline'
+        : 'star-outline',
+      rating >= 5
+        ? 'star'
+        : rating >= 4.5
+        ? 'star-half-outline'
+        : 'star-outline',
     ];
   }
 
-  public redirect(url: string){
+  public redirect(url: string) {
     window.location.replace(url);
   }
 }
