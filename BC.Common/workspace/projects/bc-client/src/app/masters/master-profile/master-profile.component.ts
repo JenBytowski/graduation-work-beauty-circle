e5981da0723f25a4b0c7711b38b278210bd7fe4d
Tsimur Kuzmich <t.kuzmich@godeltech.com>
2021-03-25 15:27:39 +0300

    Add changes

--- BC.Common/workspace/projects/bc-client/src/app/masters/master-profile/master-profile.component.ts
+++ BC.Common/workspace/projects/bc-client/src/app/masters/master-profile/master-profile.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { MasterListClient, BookingClient, JWTDecodeService, TokenStoreService} from 'bc-common';
+import { MasterListClient, BookingClient, JWTDecodeService, TokenStoreService } from 'bc-common';
 
 @Component({
   selector: 'app-master-profile',
@@ -23,6 +23,7 @@ export class MasterProfileComponent implements OnInit {
     this.masterList.getMasterById(id).subscribe((data) => {
       (data as any).starRating = this.countStarRating(data.averageRating);
       this.vm.Master = data;
+      this.vm.Schedule = this.FillSchrdule(id);
       this.vm.ClientId = this.jwtdecode.decode(this.tokenStore.get())[
         'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier'
       ];
@@ -65,6 +66,10 @@ export class MasterProfileComponent implements OnInit {
     window.location.replace(url);
   }
 
+  public getMastersSchedule(){
+    this.booking.getSchedule(this.vm.Master.id).subscribe(data => console.log(data));
+  }
+
   public addBooking() {
     let req: BookingClient.AddBookingReq = new BookingClient.AddBookingReq();
     req.masterId = this.vm.Master.id;
@@ -76,9 +81,18 @@ export class MasterProfileComponent implements OnInit {
     console.log(req);//
     this.booking.addBooking(req).subscribe((data) => console.log(data));
   }
+
+  public FillSchrdule(id: string): BookingClient.GetScheduleRes {
+    let schedule: BookingClient.GetScheduleRes;
+    this.booking.getSchedule(id).subscribe(data => schedule = data);
+    return schedule;
+   }
 }
+
+
 
 class Vm {
   public Master: any;
+  public Schedule: BookingClient.GetScheduleRes;
   public ClientId: string;
 }
