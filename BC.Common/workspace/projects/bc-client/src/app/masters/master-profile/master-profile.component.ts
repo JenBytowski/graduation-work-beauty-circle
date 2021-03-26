8e119a58e0371ebd627d2c2c5f387ea1d2acd9e5
Tsimur Kuzmich <t.kuzmich@godeltech.com>
2021-03-26 18:10:56 +0300

    Add range slider

--- BC.Common/workspace/projects/bc-client/src/app/masters/master-profile/master-profile.component.ts
+++ BC.Common/workspace/projects/bc-client/src/app/masters/master-profile/master-profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { IonRange } from '@ionic/angular';
 import { MasterListClient, BookingClient, JWTDecodeService, TokenStoreService } from 'bc-common';
 
 @Component({
@@ -10,11 +11,11 @@ import { MasterListClient, BookingClient, JWTDecodeService, TokenStoreService } 
 export class MasterProfileComponent implements OnInit {
 
   public vm: Vm = new Vm();
+  public rangeMax: number;
+  public rangeMin: number;
 
-  @ViewChild("itemDate", { static: false })
-  itemDate: ElementRef;
-  @ViewChild("itemTime", { static: false })
-  itemTime: ElementRef;
+  @ViewChild("range", { static: false })
+  range: IonRange;
 
   constructor(
     private route: ActivatedRoute,
@@ -76,34 +77,42 @@ export class MasterProfileComponent implements OnInit {
     this.booking.getSchedule(this.vm.Master.id).subscribe(data => console.log(data));
   }
 
-  public changeItemState(item: BookingClient.ScheduleDayItemRes) {
-    if (item.itemType == 'Window') {
-      let req: BookingClient.AddBookingReq = new BookingClient.AddBookingReq();
-      req.masterId = this.vm.Master.id;
-      req.clientId = this.vm.ClientId;
-      req.serviceType = "C9F93769-75F9-4C85-8D5D-D6893D6887C5";
-      req.description = 'Description';
-      item.startTime.setHours(item.startTime.getHours() + 3);
-      req.startTime = item.startTime;
-      item.endTime.setHours(item.endTime.getHours() + 3);
-      req.endTime = item.endTime;
-      console.log(req);//
-      console.log(item);//
-      //this.booking.addBooking(req).subscribe((data) => console.log(data));
-      //document.location.reload();
-    }
-    else if (item.itemType == 'Booking' && this.vm.ClientId == item.clientId) {
-      let req: BookingClient.CancelBookingReq = new BookingClient.CancelBookingReq();
-      req.bookingId = item.id;
-      console.log(req);
-      //this.booking.cancelBooking(req).subscribe(data => console.log(data));
-      //document.location.reload();
+  public changeItemState(event) {
+    if (this.vm.CurrentItem.itemType == 'Window') {
+      let range = (event.target.parentElement.childNodes[0] as IonRange).value;
+      if ((range as any).lower < (range as any).upper) {
+        let req: BookingClient.AddBookingReq = new BookingClient.AddBookingReq();
+        req.masterId = this.vm.Master.id;
+        req.clientId = this.vm.ClientId;
+        req.serviceType = "C9F93769-75F9-4C85-8D5D-D6893D6887C5";
+        req.description = 'Description';
+        this.vm.CurrentItem.startTime.setHours((range as any).lower + 3);
+        req.startTime = this.vm.CurrentItem.startTime;
+        this.vm.CurrentItem.endTime.setHours((range as any).upper + 3);
+        req.endTime = this.vm.CurrentItem.endTime;
+        console.log(req);//
+        console.log(this.vm.CurrentItem);//
+        this.booking.addBooking(req).subscribe((data) => console.log(data));
+        document.location.reload();
+      }
     }
   }
 
-  public ShowItems(): void {
-    (this.itemDate as any).el.style.display = "none";
-    (this.itemTime as any).el.style.display = "inline";
+  public enableRange(item: BookingClient.ScheduleDayItemRes) {
+    if (item.itemType == "Window") {
+      (this.range as any).el.disabled = false;
+      this.rangeMin = item.startTime.getHours();
+      this.rangeMax = item.endTime.getHours();
+      this.vm.CurrentItem = item;
+    }
+
+    if (item.itemType == 'Booking' && this.vm.ClientId == item.clientId) {
+      let req: BookingClient.CancelBookingReq = new BookingClient.CancelBookingReq();
+      req.bookingId = item.id;
+      console.log(req);
+      this.booking.cancelBooking(req).subscribe(data => console.log(data));
+      document.location.reload();
+    }
   }
 }
 
@@ -111,4 +120,5 @@ class Vm {
   public Master: any;
   public Schedule: BookingClient.GetScheduleRes;
   public ClientId: string;
+  public CurrentItem: BookingClient.ScheduleDayItemRes;
 }
